47c81b93532f886e2000d492e353e9c0db15bddb
ae589 <damnpanache@gmail.com>
2014-01-14 17:19:00 +0000

    Adding non-native fullscreen setting

--- init.el
+++ init.el
@@ -3,6 +3,9 @@
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
+(add-to-list 'default-frame-alist '(alpha . (90 70)))
+(setq ns-use-native-fullscreen nil)
+
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -21,7 +24,12 @@
                       ;; Misc.
                       markdown-mode
                       twilight-theme
-                      hlinum)
+                      hlinum
+                      color-theme
+                      exec-path-from-shell
+		      scala-mode2
+		      ensime
+		      tree-mode)
   "A list of packages to ensure are installed at launch.")
 
 ;; Automaticaly install any missing packages
@@ -29,8 +37,19 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+(require 'windata)
+(require 'dirtree)
+
+(exec-path-from-shell-initialize)
+
 ;; Load the provided Clojure start kit configurations
 (load (concat user-emacs-directory "clojure-starter-kit.el"))
+
+;;(if (eq window-system 'x) (set-face-attribute 'default nil :font "Consolas-12"))
+(set-face-attribute 'default nil :font "Consolas-12")
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -40,3 +59,10 @@
  '(font-lock-warning-face ((t (:inherit nil :foreground "red" :background nil))))
  '(linum-highlight-face ((t (:inherit default :background "color-238" :foreground "white"))))
  '(show-paren-match ((((class color) (background dark)) (:inherit nil :foreground "red")))))
+
+(load-theme 'deeper-blue t)
+
+(global-set-key [M-left] 'windmove-left)          ; move to left windnow
+(global-set-key [M-right] 'windmove-right)        ; move to right window
+(global-set-key [M-up] 'windmove-up)              ; move to upper window
+(global-set-key [M-down] 'windmove-down)          ; move to downer window
